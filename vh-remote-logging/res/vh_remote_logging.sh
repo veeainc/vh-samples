4412c101404dd117fdff6836dc900a49b94ef8c1
Michael Howell <michael.howell@veeasystems.com>
2020-07-28 16:24:06 +0200

    initial remote logging. Changes in storage example build

---
+++ vh-remote-logging/res/vh_remote_logging.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+################################################################################
+# Copyright (C) Veea Systems Limited - All Rights Reserved.
+# Unauthorised copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential. [2019-2020]
+################################################################################
+
+# temporary return so main test script does not run into errors
+exit 0
