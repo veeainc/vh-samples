33eaf3b850adf07843163ad10a78bb71eef7089d
mmardinian <mmardinian@veea.com>
2020-09-14 16:24:53 +0200

    PostGreSQL project that re-use a well-knowed alpine docker project. PLEASE: do not break this

---
+++ vh-postgresql/gen.sh
@@ -0,0 +1,4 @@
+#git clone git@github.com:onjin/docker-alpine-postgres.git
+ln -s docker-alpine-postgres/9.4/Dockerfile Dockerfile
+#ln -s docker-alpine-postgres/9.4/docker-entrypoint.sh src/docker-entrypoint.sh
+ln -s docker-alpine-postgres/9.4/docker-entrypoint.sh docker-entrypoint.sh
